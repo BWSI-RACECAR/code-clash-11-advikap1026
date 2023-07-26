5b98e7f546d8261a275149dbf3a7c1b55dc7e8ca
advikap1026 <127911640+advikap1026@users.noreply.github.com>
2023-07-26 15:46:13 -0400

    Add files via upload

--- increasingsubsequence.py
+++ increasingsubsequence.py
@@ -14,20 +14,21 @@ class Solution:
             #          answer = answer + 1
             # return answer 
             
-            if len(arr) != 0:
-                 count = 1
-                 newCount = 0
-                 for i in range(1, len(arr)):
-                      if(arr[i]>arr[i-1]):
-                           count += 1
-                      else:
-                           count = 1
-                           newCount += 1
+            if(len(arr)!=0):
+                count=1
+                newcount=0
+                for i in range(1,len(arr)):
+                    if(arr[i]>arr[i-1]):
+                        count+=1
+                    else:
+                        count=1
+                        newcount+=1
+                print(arr)
+                print(count)
             else:
-                 count = 0
-                 #newCount = 0
-            answer = count + newCount
-            return answer 
+                count=0
+            
+            return count+newcount
             pass
 
 def main():
